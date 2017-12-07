15b9e36c5acd613013345f4fdb5fd53ff0d56175
chenpengda <13241818007@163.com>
2017-12-07 14:50:03 +0800

    添加再次训练模块

--- model/retrain.py
+++ model/retrain.py
@@ -19,19 +19,43 @@ from keras.layers import Embedding
 from keras.layers import Dense, Input, Flatten
 from keras.layers import Conv1D, MaxPooling1D, Embedding, Merge, Dropout, LSTM, GRU, Bidirectional,TimeDistributed
 from keras.models import Model
-from keras.models import load_model  
+
 from keras import backend as K
 from keras.engine.topology import Layer, InputSpec
-DATA_DIR = "../data/"
+from keras.models import load_model
 
+DATA_DIR = "../data/"
+EMBEDDING_DIM = 25
+MAX_SESSION_LENGTH = 150   #the max length of all player single sessions
+MAX_SESSIONS = 300   #the max sessions length of all player
 f = open(os.path.join(DATA_DIR, 'samples'),'rb')
 #x_train, y_train = pickle.load(f)
-x_val, y_val = pickle.load(f)
+x_train, y_train = pickle.load(f)
+x_val,y_val = pickle.load(f)
 f.close()
-
-
-
 model = load_model("my_model.h5")
+
+print("model fitting - Bidirectional LSTM")
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+model.fit(x_train, y_train, validation_split = 0.1,
+          epochs=5, batch_size=20, verbose=1)
+
+
+
+model.save('my_model.h5')
+
+
 
 loss_and_metrics = model.evaluate(x_val, y_val, batch_size=20)
 print(loss_and_metrics)
+
+
+
+
+
+
+
